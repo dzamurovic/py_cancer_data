808476eb5d1791b66e5f35ce8d8c793df582bef3
ezamur <ezamur@gmail.com>
2016-01-26 16:49:27 +0100

    Playing with pandas dataframes.

--- cancer_scripts/src/processing.py
+++ cancer_scripts/src/processing.py
@@ -7,4 +7,20 @@ class DataProcessor:
     def do_the_thing(self, toProcess):
         variables = toProcess[0].keys()
         df = pd.DataFrame([[getattr(i,j) for j in variables] for i in toProcess], columns = variables)
-        #df.to_csv(path_or_buf="data_grouped.txt", sep="\t", index=False)
+
+        df_deaths = df[['age', 'gender', 'cancer', 'cumulative_risk', 'deaths']]
+        deaths_grouped = df_deaths.groupby(['age', 'gender'], sort=True)
+        deaths_summed = deaths_grouped.sum()
+        #print(deaths_summed)
+
+        male_occurrences = df_deaths[df_deaths['gender'] == 'Male']
+        #print(male_occurrences)
+
+        # how to split in two data frames by gender by creating a dictionary that will contain those dataframes
+        genders = df.gender.unique()
+        data_frame_dict = {elem : df for elem in genders}
+        for key in data_frame_dict.keys():
+            data_frame_dict[key] = df[:][df.gender == key]
+
+        print(data_frame_dict)
+
